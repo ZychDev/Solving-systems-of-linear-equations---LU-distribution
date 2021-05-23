1cf4b923d352a771a0d5a1134e2960b84948a87e
ZychDev <szymonzychdev@gmail.com>
2021-05-24 01:13:14 +0200

    file opening protection

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
 #include<vector>
-
+#include<fstream>
 
 int main()
 {
+    //file check
+    std::ifstream my_file("sample.txt");
+    
+    try{
+        bool check = true;
+        if(my_file.is_open())
+        {   
+            std::cout<<"file is ok"<<std::endl;
+        }
+        else
+        {
+            check = false;
+            throw(check);
+        }
+    }
+    catch(bool my_check)
+    {
+        std::cout<<"Wrong input file"<<std::endl;
+    }
+    
 
     return 0;
 }
